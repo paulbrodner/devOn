a604bbcfa9154509586ce388b0266609e5fccebb
Paul Brodner <paul.brodner@ness.com>
2017-11-25 10:26:13 +0200

    v2.0.0 initialize

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-[ -f pkg/devOn-1.0.2.gem ] && rm pkg/devOn-1.0.2.gem
+VERSION=2.0.0
+[ -f pkg/devOn-${VERSION}.gem ] && rm pkg/devOn-${VERSION}.gem
 gem uninstall devOn
 rake build
-gem install pkg/devOn-1.0.2.gem
+gem install pkg/devOn-${VERSION}.gem --no-ri --no-rdoc
